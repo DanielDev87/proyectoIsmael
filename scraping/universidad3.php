391f1b4b2dafd25991d8fd4445731877424afa1a
Ismaelcortes <162367576+ismasexo@users.noreply.github.com>
2024-11-09 14:40:21 -0500

    Update universidad3.php

---
+++ scraping/universidad3.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'vendor/autoload.php';
+use Goutte\Client;
+
+$client = new Client();
+$crawler = $client->request('GET', 'https://universidad3.edu.co/programas');
+
+$programas = [];
+$crawler->filter('.programa-clase')->each(function ($node) use (&$programas) {
+    $programas[] = [
+        'nombre' => 'Universidad 3',
+        'carrera' => $node->filter('.nombre')->text(),
+        'precio' => $node->filter('.precio')->text(),
+        'materias' => explode(", ", $node->filter('.materias')->text()),
+        'becas' => $node->filter('.becas')->text(),
+        'link' => $node->filter('a')->attr('href'),
+        'ciudad' => 'Medellín',
+        'sedes' => ['Sede Principal']
+    ];
+});
+
+file_put_contents('../data/universidades.json', json_encode($programas));

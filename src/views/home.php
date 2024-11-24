a247800bda55f44db0f3284bf28527bbc822192a
Ismael Cortès Betancur <cortesbetancurismael@gmail.com>
2024-11-24 18:27:22 -0500

    Modificacion de home

--- src/views/home.php
+++ src/views/home.php
@@ -4,9 +4,59 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Welcome to the Home Page</h1>
-    <p>This is the home page of the project.</p>
+    <div class="container py-3">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of the project.</p>
+        
+        <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm h-100">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal">Careras</h4>
+                    </div>
+                    <div class="card-body">
+                        <font style="vertical-align: inherit;">Descripcion general de la carrera que deseas</font>
+                    </div>
+                    <div class="card-footer">
+                        
+                        <a href="career/select.php" class="w-100 btn btn-lg btn-primary mt-auto">Gestionar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm h-100">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal">Universidades</h4>
+                    </div>
+                    <div class="card-body">
+                        <font style="vertical-align: inherit;">Encuentra datos esenciales, ya teniendo tu carrera deseada, para tener mayor comodidad a la hora de escoger tu univercidad</font>
+                    </div>
+                    <div class="card-footer">
+                        <a href="university/select.php" class="w-100 btn btn-lg btn-primary mt-auto">Gestionar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sm h-100">
+                    <div class="card-header py-3">
+                        <h4 class="my-0 fw-normal">Usuario</h4>
+                    </div>
+                    <div class="card-body">
+                        <font style="vertical-align: inherit;">Ingresa tus datos para poder navegar por esta plataforma</font>
+                    </div>
+                    <div class="card-footer">
+                        <a href="user/select.php" class="w-100 btn btn-lg btn-primary mt-auto">Gestionar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
